c48a6872e5448fe8b14446be842d7c2da929d2a5
Jens Holm <jensgholm15@gmail.com>
2017-07-21 16:12:50 +0200

    Add per frame recompute settings script

---
+++ Assets/Scripts/RecomputePerFrame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LensFlaresMatrixMethod))]
+public class RecomputePerFrame : MonoBehaviour
+{
+
+    LensFlaresMatrixMethod m_FlareComponent;
+
+    public void OnEnable()
+    {
+        m_FlareComponent = GetComponent<LensFlaresMatrixMethod>();
+    }
+
+    public void Update()
+    {
+        m_FlareComponent.aperatureEdges = 5 + Time.renderedFrameCount % 2;
+        m_FlareComponent.Clean();
+    }
+
+}
